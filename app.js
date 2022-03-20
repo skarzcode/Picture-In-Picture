27733aebc0257db820362acf44c954d502f9ae39
Abdi <letsbuildshhopify>
2022-03-20 13:48:50 +0000

    js Notes

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const videoElement = document.getElementById('video');
 
 async function selectMedia() {
 try{
+    // promts user to select a window to share
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    // passes that shared window into the src of html video element
     videoElement.srcObject = mediaStream;
+    // once duration, size and dimensions have been determeined the video can start playing
     videoElement.onloadedmetadata = () => {
         videoElement.play();
     }
@@ -16,7 +19,9 @@ try{
 };
 
 button.addEventListener('click', async () => {
+    // once clicked it disables the button and no longer can be used
     button.disabled = true;
+    // once pictureinpicture is successfull the button is then active and functional. If not the button wont function untill the error is resolved.
     await videoElement.requestPictureInPicture();
     button.disabled = false;
 })
